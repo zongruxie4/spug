779d029044b357470943440c9ae9b90b164cb729
vapao <live1989@foxmail.com>
2026-06-12 11:04:36 +0800

    fix(auth): escape ldap login filter values

--- spug_api/libs/ldap.py
+++ spug_api/libs/ldap.py
@@ -2,6 +2,7 @@
 # Copyright: (c) <spug.dev@gmail.com>
 # Released under the AGPL-3.0 License.
 import ldap
+from ldap.filter import escape_filter_chars
 
 
 class LDAP:
@@ -17,7 +18,7 @@ class LDAP:
         try:
             conn = ldap.initialize("ldap://{0}:{1}".format(self.server, self.port), bytes_mode=False)
             conn.simple_bind_s(self.admin_dn, self.password)
-            search_filter = f'({self.rules}={username})'
+            search_filter = f'({self.rules}={escape_filter_chars(username)})'
             ldap_result_id = conn.search(self.base_dn, ldap.SCOPE_SUBTREE, search_filter, None)
             result_type, result_data = conn.result(ldap_result_id, 0)
             if result_type == ldap.RES_SEARCH_ENTRY:
